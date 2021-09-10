8e54457a4d3aa301485fa0e89ea4460cac4c58d9
Colin Gregurich <coling112198@gmail.com>
2021-09-09 21:10:36 -0500

    added easter egg

--- script.js
+++ script.js
@@ -16,21 +16,36 @@ function saveContrastMode(){
 function showSum(){
   const values = input.value.split("\n");
   const sum = values.reduce((sum, val) => parseInt(sum)+parseInt(val));
-  console.log(sum);
   if (!isNaN(sum)){
     value.innerText = sum;
   }
 }
 
+function displayTanner(){
+  console.log("tanner applied");
+  input.classList.add("tanner");
+}
+
+function hideTanner(){
+  input.classList.remove("tanner");
+}
+
 function keyPressEvent(e){
+  console.log(e);
+  if (e.ctrlKey) e.preventDefault();
   if (e.code == "Enter" && e.ctrlKey){
+    console.log(input.value.toLowerCase().trim());
+    if (input.value.toLowerCase().trim() == "tanner") displayTanner();
+    else hideTanner();
     showSum();
   }
-  if (e.code == "Equal" && e.ctrlKey){
-    e.preventDefault();
+  if (e.code == "Equal"){
     increaseInputFontSize();
+    console.log("equal");
+    e.preventDefault();
   }
   if (e.code == "Minus" && e.ctrlKey){
+    console.log("minus");
     e.preventDefault();
     decreaseInputFontSize();
   }
@@ -82,6 +97,8 @@ function enableDarkMode(){
   calculateButton.classList.add("dark");
   adjustIcon.style.color = "white";
   helpIcon.style.color = "white";  
+  plusIcon.style.color = "white";
+  minusIcon.style.color = "white";
 }
 
 function enableLightMode(){
@@ -94,6 +111,8 @@ function enableLightMode(){
   calculateButton.classList.add("light");
   adjustIcon.style.color = "black";
   helpIcon.style.color = "black";
+  plusIcon.style.color = "black";
+  minusIcon.style.color = "black";
 }
 
 let contrastMode = "light";
@@ -105,9 +124,13 @@ const contrastButton = document.querySelector(".light-dark");
 const calculateButton = document.querySelector(".calculate");
 const adjustIcon = document.querySelector(".fa-adjust");
 const helpIcon = document.querySelector(".fa-question-circle");
+const plusIcon = document.querySelector(".fa-plus");
+const minusIcon = document.querySelector(".fa-minus");
 
 document.querySelector(".calculate").addEventListener("click", showSum);
 document.addEventListener("keypress", keyPressEvent);
 adjustIcon.addEventListener("click", switchContrastMode);
+plusIcon.addEventListener("click", increaseInputFontSize);
+minusIcon.addEventListener("click", decreaseInputFontSize);
 
 loadFromLocalStorage();
